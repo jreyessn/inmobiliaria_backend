31ad312fa239da8a846f2da2dd6e276c44fdd8ef
Juan Reyes <snjuank@gmail.com>
2022-02-03 14:35:07 -0400

    Ajustar height de logo de mails

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ $url }}public/logo.png" class="logo" alt="Logo" style="width: 200px; height: 70px;">
+<img src="{{ $url }}public/logo.png" class="logo" alt="Logo" style="width: 200px; height: 120px;">
 </a>
 </td>
 </tr>

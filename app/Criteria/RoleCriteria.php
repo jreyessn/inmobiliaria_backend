6495f932c42d5b676720b5623683013799b464db
Juan Reyes <snjuank@gmail.com>
2022-02-21 18:42:05 -0400

    Criterio de roles por nombre

--- app/Criteria/RoleCriteria.php
+++ app/Criteria/RoleCriteria.php
@@ -23,12 +23,21 @@ class RoleCriteria implements CriteriaInterface
     public function apply($model, RepositoryInterface $repository)
     {
         $roles = request()->get("role_id", null);
+        $rolesNames = request()->get("role", null);
 
         if($roles){
             $rolesList = explode(",", $roles);
 
             $model = $model->whereHas("roles", function($query) use ($rolesList){
                 $query->whereIn("id", $rolesList);
+            });
+        }
+
+        if($rolesNames){
+            $rolesList = explode(",", $rolesNames);
+            
+            $model = $model->whereHas("roles", function($query) use ($rolesList){
+                $query->whereIn("name", $rolesList);
             });
         }
 
